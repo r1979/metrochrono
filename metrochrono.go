31fa7da9a72de24444de4a4f39bbb131857bad25
Ruggero M. <r1979@users.noreply.github.com>
2025-04-22 12:42:18 +0200

    Update metrochrono.go

--- metrochrono.go
+++ metrochrono.go
@@ -294,7 +294,7 @@ func main() {
 		// Get the ID for button callbacks
 		id := i // Important: Create a new variable to capture the current value of i
 
-		startButton := tview.NewButton("Start").SetLabelColor("Green").SetSelectedFunc(func() {
+		startButton := tview.NewButton("Start").SetLabelColor(#00ff00).SetSelectedFunc(func() {
 			manager.StartChronometer(id)
 		})
 
